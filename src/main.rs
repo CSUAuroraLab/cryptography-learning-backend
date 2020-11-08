1d19f4018fe85669e85ea63bcc7f58f54b8292d5
CSUwangj <CSUwangj@protonmail.com>
2020-11-08 16:05:37 +0800

    feat(route): more gentle routes
    with prettier logger

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,12 @@ extern crate thiserror;
 
 use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
 use async_graphql::{EmptyMutation, EmptySubscription, Schema};
+use async_graphql_warp::{Response};
 use dotenv::dotenv;
-use log::{debug, error, info, warn};
-use structopt::StructOpt;
-use async_graphql_warp::{BadRequest, Response};
-use http::StatusCode;
+use log::{debug, error};
 use std::convert::Infallible;
-use warp::{http::Response as HttpResponse, Filter, Rejection};
-
+use structopt::StructOpt;
+use warp::{http::Response as HttpResponse, Filter};
 
 mod errors;
 mod model;
@@ -29,49 +27,36 @@ async fn main() {
         3 => std::env::set_var("RUST_LOG", "debug"),
         _ => std::env::set_var("RUST_LOG", "trace"),
     }
-    env_logger::init();
-    debug!("{:?}", opt);
-    let static_file_path = opt.static_file_path;
+    pretty_env_logger::init();
 
+    debug!("{:?}", opt);
     let config = Configuration::from_file(opt.config);
     debug!("{:?}", config);
 
-    print!("Playground: http://localhost:8000/");
+    print!("Playground: http://localhost:8000/playground");
 
     let schema = Schema::build(Query, EmptyMutation, EmptySubscription)
         .data(config.clone())
         .finish();
 
-    println!("Playground: http://localhost:8000");
-
-    let graphql_post = async_graphql_warp::graphql(schema).and_then(
+    let graphql_post = warp::path("query")
+        .and(async_graphql_warp::graphql(schema))
+        .and_then(
         |(schema, request): (
             Schema<Query, EmptyMutation, EmptySubscription>,
             async_graphql::Request,
         )| async move { Ok::<_, Infallible>(Response::from(schema.execute(request).await)) },
     );
 
-    let graphql_playground = warp::path::end().and(warp::get()).map(|| {
+    let graphql_playground = warp::path("playground").map(|| {
         HttpResponse::builder()
             .header("content-type", "text/html")
-            .body(playground_source(GraphQLPlaygroundConfig::new("/")))
+            .body(playground_source(GraphQLPlaygroundConfig::new("/query")))
     });
 
-    let routes = graphql_playground
-        .or(graphql_post)
-        .recover(|err: Rejection| async move {
-            if let Some(BadRequest(err)) = err.find() {
-                return Ok::<_, Infallible>(warp::reply::with_status(
-                    err.to_string(),
-                    StatusCode::BAD_REQUEST,
-                ));
-            }
+    let static_files = warp::path("static").and(warp::fs::dir(opt.static_file_path));
 
-            Ok(warp::reply::with_status(
-                "INTERNAL_SERVER_ERROR".to_string(),
-                StatusCode::INTERNAL_SERVER_ERROR,
-            ))
-        });
+    let routes = graphql_playground.or(static_files).or(graphql_post);
 
     warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
 }
