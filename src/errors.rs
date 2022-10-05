289023435c021645417df6fe99e3d632a4bfa5a1
CSUwangj <CSUwangj@protonmail.com>
2022-10-05 20:57:58 +0800

    fix: update dependencies

--- src/errors.rs
+++ src/errors.rs
@@ -12,7 +12,7 @@ pub enum QueryError {
 
 impl ErrorExtensions for QueryError {
     // lets define our base extensions
-    fn extend(&self) -> FieldError {
+    fn extend(self) -> FieldError {
         self.extend_with(|err, e| match err {
             QueryError::NotFoundError(resource) => e.set("code", format!("{} not found", resource)),
             QueryError::ServerError(reason) => e.set("reason", reason.to_string()),
